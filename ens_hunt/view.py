bc2de6212c0382d51d60f13e5d2b49b00a0c2382
Hoàng Anh Lâm <comficker@gmail.com>
2022-07-25 14:36:06 +0700

    Init

--- ens_hunt/view.py
+++ ens_hunt/view.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from app.models import Asset
+from app.models import Asset, Report
 from datetime import datetime
 
 
@@ -10,9 +10,10 @@ def sitemap_style(request):
 
 
 def sitemap_index(request):
-    sm = [
-        "https://demask.io/asset-sitemap.xml",
-    ]
+    reports = Report.objects.values("created").distinct()
+    sm = []
+    for item in reports:
+        sm.append("https://enshunt.com/{}-sitemap.xml".format(item["created"].strftime('%Y-%m-%d')))
     template = loader.get_template('./sitemap_index.xml')
     return HttpResponse(template.render({
         "sitemaps": sm
@@ -21,18 +22,20 @@ def sitemap_index(request):
 
 def sitemap_detail(request, flag):
     template = loader.get_template('./sitemap.xml')
-    if flag == "asset":
-        ds = list(map(
-            lambda x: {
-                "location": "https://enshunt.com/{}".format(x.item_id),
-                "priority": 0.8,
-                "updated": datetime.now(),
-                "changefreq": "daily"
-            },
-            Asset.objects.all()
-        ))
-    else:
-        ds = []
+    date = datetime.strptime(flag, "%Y-%m-%d").date()
+    ds = list(map(
+        lambda x: {
+            "location": "https://enshunt.com/{}-{}.eth".format(x.id, x.name),
+            "priority": 0.8,
+            "updated": datetime.now(),
+            "changefreq": "daily"
+        },
+        Asset.objects.filter(
+            mint_date__day=date.day,
+            mint_date__month=date.month,
+            mint_date__year=date.year
+        )
+    ))
     return HttpResponse(template.render({
         "dataset": ds
     }, request), content_type='text/xml')
